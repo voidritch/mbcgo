285d6cdc52aa1e9a2852e4dd2007e87512767ee7
Ian W <wangyinan@ymail.com>
2017-06-29 09:00:54 +1200

    Update server.go

--- server.go
+++ server.go
@@ -7,8 +7,11 @@ import (
 )
 
 func main() {
-        fs := http.FileServer(http.Dir("static"))
-        http.Handle("/", fs)
+        // fs := http.FileServer(http.Dir("static"))
+	root := func (w http.ResponseWriter, r *http.Request) {
+    		fmt.Fprintf(w, "You have reached %s!", r.URL.Path[1:])
+	}
+        http.Handle("/", root)
 
         log.Println("Listening...")
         http.ListenAndServe(":8080", nil)
